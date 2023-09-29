ef270500dd91be8e8d6b072a145864c8cb28cb42
Maria Marin <mdmarin1@miuandes.cl>
2023-09-29 05:04:57 -0300

    cambio por definir

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,8 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
-
+¿
   resources :pacients
   resources :institutions
   resources :medical_centers
-
-=======
-  resources :pacients
-  resources :institutions
-  resources :medical_centers
->>>>>>> 888aea9d734d76184abc80d34554df25b06f8b3e
   resources :doctors
   resources :appointments
   root 'posts#index'
